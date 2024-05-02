71676a36d20a16e4d6a7f96d05203986b4ff15e6
PersianDavinci <persiandavinci@gmail.com>
2024-05-02 18:13:16 +0330

    change dashboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -36,7 +36,7 @@ async function page() {
                   variant={"outline"}
                   className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
                 >
-                  New income 🤑
+                  New income 💵
                 </Button>
               }
               type="income"
@@ -48,7 +48,7 @@ async function page() {
                   variant={"outline"}
                   className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
                 >
-                  New expense 😤
+                  New expense 🛒
                 </Button>
               }
               type="expense"
